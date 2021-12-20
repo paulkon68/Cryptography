172d609a5ce45c8b51d95557ba39906b7999bde2
PaulKon <paulkon68@gmail.com>
2021-12-20 15:00:33 +0200

    Update caesar.py

--- caesar.py
+++ caesar.py
@@ -1,14 +1,10 @@
-def decrypt(coordinates, key=3):
-    for num in coordinates:
+def decrypt(numeric_list, key=3):
+    for num in numeric_list:
         p = (num-key) % 26
         print(p, end=' ')
     print('\n')
 
 
-coordinates1 = [17, 0, 34]
-coordinates2 = [68, 2, 48]
-
 for i in range(1, 26):
     print(f'[+] Key number: {i}')
-    decrypt(coordinates1, i)
-    decrypt(coordinates2, i)
+    decrypt([1, 2, 3, 4, 5], i)

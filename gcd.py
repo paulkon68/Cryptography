f85a1b796f61384a5a8f685cad68b5779eb2d576
PaulKon <paulkon68@gmail.com>
2022-11-21 21:24:26 +0200

    Update gcd.py

--- gcd.py
+++ gcd.py
@@ -11,10 +11,10 @@ def gcd(num1, num2):
         return num2
     if num2 == 0:
         return num1
-    if num1 > num2:
+    if num2 > num1:
         return gcd(num2, num1)
     else:
-        return gcd(num1, num2 % num1)
+        return gcd(num2, num2 % num1)
 
 
 print(gcd(sys.argv[1], sys.argv[2]))
